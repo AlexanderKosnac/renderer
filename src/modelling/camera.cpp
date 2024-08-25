1d3d0060f250fd8d76c307939d4e6359baaef16d
Alexander Kosnac <alexander.kosnac@web.de>
2024-08-25 23:10:57 +0200

    Camera transformation in camera

--- src/modelling/camera.cpp
+++ src/modelling/camera.cpp
@@ -1,6 +1,8 @@
 #include "math.h"
 #include "modelling/camera.h"
 
+#include "math.h"
+
 namespace modelling {
 
     Camera::Camera(float pFov, float pAspect, float pNear, float pFar, math::vec3 pPos, math::vec3 pUp, math::vec3 pLookat) {
@@ -15,6 +17,10 @@ namespace modelling {
         u = math::crossVec3(up, lookat);
         v = math::crossVec3(lookat, u);
         w = lookat;
+    }
+
+    math::mat3x3 Camera::getCameraTransformation() {
+        return math::mat3x3(pos, lookat, up);
     }
 
     math::vec3& Camera::getPos() {
